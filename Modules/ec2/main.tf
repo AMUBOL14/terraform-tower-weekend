a522adfaae937d109e895fc833a1fa4dd6925bae
unknown <amubol14@gmail.com>
2023-08-25 23:09:19 +0100

    first commit

---
+++ Modules/ec2/main.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "myec2" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+  subnet_id = var.subnet_id
+  tags = {
+    name = "Helloworld"
+  }
+}
---
+++ Modules/ec2/variables.tf
@@ -0,0 +1,14 @@
+variable "instance_type" {
+  default = ""
+  type = string
+}
+
+variable "subnet_id" {
+  default = ""
+  type = string
+}
+
+variable "env" {
+  default = ""
+  type = string
+}
